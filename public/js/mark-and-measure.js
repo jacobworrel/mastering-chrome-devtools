1aefbcb8426270ddcf457d534b8eaf42065378b3
jacobworrel <jacobworrel@gmail.com>
2019-04-30 19:25:16 -0700

    complete performance measuring ex

--- public/js/mark-and-measure.js
+++ public/js/mark-and-measure.js
@@ -1,9 +1,18 @@
 window.addEventListener("load", thingToMeasure);
 
 function thingToMeasure() {
+  performance.mark('start');
   fetch("/api")
     .then(result => result.json())
     .then(json => {
       json.images.forEach(image => console.log(image.name));
+      performance.mark('end');
+      performance.measure('Fetching Images', 'start', 'end');
+      // performance.getEntriesByType('measure');
+      const { duration } = performance.getEntriesByType('measure')[0];
+      console.info('time in ms:', duration);
+      performance.clearMarks();
+      performance.clearMeasures();
     });
+
 }
